f81825d9f5b8c10860acf4b116debceefb332408
omjdreams <om.javheri.dreams2@gmail.com>
2026-05-19 14:35:59 +0100

    sync

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+// import Home from './components/Home'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-             
-           <Navbar/>
-           <main >
-           <Outlet/>
+      <Navbar />
+      <main>
+        <Outlet />
       </main>
-       <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
